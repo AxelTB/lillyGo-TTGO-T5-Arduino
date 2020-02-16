bb8f481440ae61b86c1548094b510e6608e805b8
Alessio Graziano <alessio.graziano@shinsekai.it>
2020-02-16 22:42:36 +0100

    cleaning

--- README.md
+++ README.md
@@ -5,6 +5,14 @@ Feature test for lillyGo TTGO T5 board on arduino
 * GxEPD library https://github.com/ZinggJM/GxEPD.git
 * Adafruit Gfx https://github.com/adafruit/Adafruit-GFX-Library.git
 
+### Pin
 37 - Button 2
 38 - Button 1
 39 - Button 3
+
+### Checklist
+* Buttons : OK
+* E-ink:    OK
+* SD:       TODO
+* Battery:  TODO
+* Buzzer:   TODO
--- lillyGo-TTGO-T5-Arduino.ino
+++ lillyGo-TTGO-T5-Arduino.ino
@@ -1,19 +1,4 @@
-//e-ink---------------------------------------------------------------
-#include "GxEPD.h"
-// select the display class to use, only one
-#include <GxGDEP015OC1/GxGDEP015OC1.h> // 1.54" b/w
-// FreeFonts from Adafruit_GFX
-#include <Fonts/FreeMonoBold9pt7b.h>
-#include <Fonts/FreeMonoBold12pt7b.h>
-#include <Fonts/FreeMonoBold24pt7b.h>
-#include <Fonts/FreeSansBoldOblique9pt7b.h>
-#include <GxIO/GxIO_SPI/GxIO_SPI.h>
-#include <GxIO/GxIO.h>
-
-GxIO_Class io(SPI, /*CS=5*/ SS, /*DC=*/17, /*RST=*/16);
-GxEPD_Class display(io, /*RST=*/16, /*BUSY=*/4);
-
-#define UPDATE_SCREEN_EVERY_S 5
+#include "eink.h"
 
 //Button configuration------------------------------------------------
 #define BUTTON_1 38
@@ -37,56 +22,21 @@ void setup()
     pinMode(buttons[i], INPUT_PULLUP);
   }
   //Display--------------------
-  display.init(); // enable diagnostic output on Serial
-  display.setRotation(3);
+  init_e_ink();
 }
 void loop()
 {
-  testButtons();
-  testVersion();
-  displayTest();
+  test_buttons();
+  test_version();
+  test_display();
 }
 
-void showFont(const char name[], const GFXfont *f)
-{
-  display.fillScreen(GxEPD_WHITE);
-  display.setTextColor(GxEPD_BLACK);
-  display.setFont(f);
-  display.setCursor(0, 0);
-  display.println();
-  display.println(name);
-  display.println(" !\"#$%&'()*+,-./");
-  display.println("0123456789:;<=>?");
-  display.println("@ABCDEFGHIJKLMNO");
-  display.println("PQRSTUVWXYZ[\\]^_");
-#if defined(HAS_RED_COLOR)
-  display.setTextColor(GxEPD_RED);
-#endif
-  display.println("`abcdefghijklmno");
-  display.println("pqrstuvwxyz{|}~ ");
-  display.update();
-}
 
-/**
- * @brief Show text on screem
- * 
- */
-void displayTest()
-{
-  static uint32_t lastUpdate = millis();
-  if (millis() - lastUpdate > ((uint32_t)UPDATE_SCREEN_EVERY_S * 1000))
-  {
-    //showFont("FreeMonoBold9pt7b", &FreeMonoBold9pt7b);
-    showFont("FreeMonoBold12pt7b", &FreeMonoBold12pt7b);
-    //showFont("FreeMonoBold24pt7b", &FreeMonoBold24pt7b);
-    lastUpdate=millis();
-  }
-}
 /**
  * @brief Checks button and write on serial when pressed
  * 
  */
-void testButtons()
+void test_buttons()
 {
   for (int i = 0; i < BUTTON_NUM; i++)
   {
@@ -104,7 +54,7 @@ void testButtons()
  * Check by touching the GPIO, if first value reacts you have v1.0 if it's the second on the version is 1.2
  * 
  */
-void testVersion()
+void test_version()
 {
   static uint32_t lastRead = millis();
   if (millis() - lastRead > READ_ANALOG_EVERY_MS)
