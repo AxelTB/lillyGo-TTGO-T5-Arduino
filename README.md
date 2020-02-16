ae3c0008a1d90dd74f02eda6d62871cf76ccc4cc
Alessio Graziano <alessio.graziano@shinsekai.it>
2020-02-16 21:17:48 +0100

    Button test with digitalRead Version check

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # lillyGo-TTGO-T5-Arduino
 Feature test for lillyGo TTGO T5 board on arduino
+
+37 - Button 2
+38 - Button 1
+39 - Button 3
--- lillyGo-TTGO-T5-Arduino.ino
+++ lillyGo-TTGO-T5-Arduino.ino
@@ -2,32 +2,57 @@
 #define BUTTON_1 38
 #define BUTTON_2 37
 #define BUTTON_3 39
+#define BUTTON_NUM 3
 
-void IRAM_ATTR ButtonISR() {
-    Serial.println("Button");
-}
+#define READ_ANALOG_EVERY_MS 100
 
-int pins[]={BUTTON_1,BUTTON_2,BUTTON_3};
+const int buttons[]={BUTTON_1,BUTTON_2,BUTTON_3};
 void setup()
 {
-    //Serial usb test
+    //Serial usb
     Serial.begin(115200);
     Serial.println("Hello world!");
-    //Button ISR
-    attachInterrupt(BUTTON_1, ButtonISR, FALLING);
+    //Button
+    //Activate pullup for buttons
+    for(int i=0; i<BUTTON_NUM; i++){
+        pinMode(buttons[i], INPUT_PULLUP);
+    }
 
 }
 
 void loop()
 {
-    Serial.println("Loop");
-    dumpStatuses(pins,3);
-    delay(1000);
+    testButtons();
+    testVersion();
+    delay(100);
+}
+/**
+ * @brief Checks button and write on serial when pressed
+ * 
+ */
+void testButtons(){
+    for (int i=0; i<BUTTON_NUM; i++){
+        if(digitalRead(buttons[i])== LOW){
+            Serial.print("Pressed button ");
+            Serial.println(i+1);
+        }
+    }
 }
 
-void dumpStatuses(int *pins, int len){
-    for(int i=0; i<len; i++){
-        Serial.print(digitalRead(pins[i])==HIGH ? "H":"L");
-    }
+/**
+ * @brief Dumps analog value of pin 25 and 33
+ * 
+ * Check by touching the GPIO, if first value reacts you have v1.0 if it's the second on the version is 1.2
+ * 
+ */
+void testVersion()
+{
+    static uint32_t lastRead=millis();
+    if(millis()-lastRead > READ_ANALOG_EVERY_MS){
+    Serial.print(analogRead(25));
+    Serial.print(", ");
+    Serial.print(analogRead(33));
     Serial.println();
+    lastRead=millis();
+    }
 }
